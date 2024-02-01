3e42bb84c0bbfabe62fccc33fb14bf7cd85122f9
Uliana Savitskaua <uljasha.sav@gmail.com>
2024-02-01 13:55:44 +0600

    add starting template and structure

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+export function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>123</p>
+      </header>
+
+      <footer></footer>
+    </div>
+  );
+}
